47bc059317dac4e82c2d5e5255ef2db6c8b274ab
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-03-05 16:26:19 +0100

    List of disruptions content

--- web-app/scripts/header.js
+++ web-app/scripts/header.js
@@ -1,3 +1,4 @@
+// SEARCH OPEN/CLOSE
 // Sélection des éléments DOM
 const inputSearch = document.querySelector('.input-search');
 const btnSearch = document.querySelector('.btn-search');
@@ -13,4 +14,25 @@ btnSearch.addEventListener('click', function() {
     } else {
         inputSearch.classList.add('open');
     }
+});
+
+// Ajout d'un écouteur d'événement pour l'ouverture du contenu des perturbations
+const lineBoxes = document.querySelectorAll('.line-box.interactive');
+// Pour chaque élément .line-box.interactive, ajouter un écouteur d'événement pour le clic sur .down-arrow
+lineBoxes.forEach(function(lineBox) {
+    console.log(lineBox);
+    const downArrow = lineBox.querySelector('.down-arrow');
+    const content = lineBox.querySelector('.content');
+
+    downArrow.addEventListener('click', function() {
+        const isOpen = content.classList.contains('open');
+
+        if (isOpen) {
+            content.classList.remove('open');
+            downArrow.classList.remove('open');
+        } else {
+            content.classList.add('open');
+            downArrow.classList.add('open');
+        }
+    });
 });
