bd45a11d76650ee245c29d96d0d2455ac2758056
E226772R <alexys.gromard@etu.univ-nantes.fr>
2024-03-19 10:59:48 +0100

    Footer and Updates

--- web-app/scripts/script.js
+++ web-app/scripts/script.js
@@ -1,21 +1,3 @@
-// SEARCH OPEN/CLOSE
-// Sélection des éléments DOM
-const inputSearch = document.querySelector('.input-search');
-const btnSearch = document.querySelector('.btn-search');
-
-// Ajout d'un écouteur d'événement pour le clic sur le bouton de recherche
-btnSearch.addEventListener('click', function() {
-    // Vérifier si l'input est actuellement ouvert
-    const isOpen = inputSearch.classList.contains('open');
-
-    // Si l'input est ouvert, le fermer ; sinon, l'ouvrir
-    if (isOpen) {
-        inputSearch.classList.remove('open');
-    } else {
-        inputSearch.classList.add('open');
-    }
-});
-
 // Ajout d'un écouteur d'événement pour l'ouverture du contenu des perturbations
 const lineBoxes = document.querySelectorAll('.line-box.interactive');
 // Pour chaque élément .line-box.interactive, ajouter un écouteur d'événement pour le clic sur .down-arrow
