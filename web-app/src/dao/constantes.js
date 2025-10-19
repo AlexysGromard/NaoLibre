d2a1858d82ed63745f5867a4bbef48c7425f5597
E226772R <alexys.gromard@etu.univ-nantes.fr>
2025-10-19 14:42:40 +0200

    Ajout des variables d'environnement pour les URLs des APIs dans le fichier .env et mise à jour du Dockerfile et des constantes pour les utiliser

--- web-app/src/dao/constantes.js
+++ web-app/src/dao/constantes.js
@@ -1,4 +1,5 @@
-const baseURLTan = 'http://localhost:8080/tan/' // Base URL for the TAN API
-const baseURLUser = 'http://localhost:8081/NaoLibre/' // Base URL for the user API
+// Utilise les variables d'environnement ou localhost par défaut
+const baseURLTan = process.env.REACT_APP_API_TAN_URL || 'http://localhost:8080/tan/'
+const baseURLUser = process.env.REACT_APP_API_USER_URL || 'http://localhost:8081/NaoLibre/'
 
 export {baseURLTan,baseURLUser}
